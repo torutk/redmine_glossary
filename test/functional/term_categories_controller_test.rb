05fddbdade473f28797af17e846c303b333b5f84
Toru Takahashi <torutk@gmail.com>
2018-07-21 22:00:53 +0900

    Make functional test for term_categories. Remove unuse test file

--- test/functional/term_categories_controller_test.rb
+++ test/functional/term_categories_controller_test.rb
@@ -1,8 +1,17 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class TermCategoriesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :projects, :users, :roles, :members, :member_roles
+
+  def setup
+    @project = projects('projects_001')
+    @project.enabled_module_names = [:glossary]
+    roles('roles_001').add_permission! :manage_term_categories
+  end
+
+  def test_index_response
+    @request.session[:user_id] = users('users_002').id
+    get :index, params: { project_id: 1 }
+    assert_response :success
   end
 end
