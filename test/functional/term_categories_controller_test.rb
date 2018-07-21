b25bf663353e9989f7647a54f1bf390f9fd5820e
Toru Takahashi <torutk@gmail.com>
2018-07-22 07:08:55 +0900

    Make functional test for term_categories with edit and change_order action

--- test/functional/term_categories_controller_test.rb
+++ test/functional/term_categories_controller_test.rb
@@ -1,7 +1,8 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class TermCategoriesControllerTest < ActionController::TestCase
-  fixtures :projects, :users, :roles, :members, :member_roles
+  fixtures :projects, :users, :roles, :members, :member_roles, :term_categories
+  plugin_fixtures :term_categories
 
   def setup
     @project = projects('projects_001')
@@ -13,5 +14,20 @@ class TermCategoriesControllerTest < ActionController::TestCase
     @request.session[:user_id] = users('users_002').id
     get :index, params: { project_id: 1 }
     assert_response :success
+  end
+
+  def test_edit
+    @request.session[:user_id] = users('users_002').id
+    patch :edit, params: { id: 1, project_id: 1, category: { name: 'eins' } }
+    category = TermCategory.find(1)
+    assert_redirected_to controller: :term_categories, project_id: @project.identifier
+    assert_equal 'eins', category.name
+  end
+
+  def test_change_order
+    @request.session[:user_id] = users('users_002').id
+    post :change_order, params: { id: 1, project_id: 1, position: 'lower' }
+    category = TermCategory.find(1)
+    assert_equal 2, category.position
   end
 end
