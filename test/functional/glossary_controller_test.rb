fa764aa16642949a873233e22dc04fcc9b757cbf
Toru Takahashi <torutk@gmail.com>
2018-07-23 06:51:43 +0900

    Make minimal functional test for glossary_controller

--- test/functional/glossary_controller_test.rb
+++ test/functional/glossary_controller_test.rb
@@ -1,8 +1,18 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class GlossaryControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :projects, :users, :roles, :members, :member_roles
+  plugin_fixtures :terms
+
+  def setup
+    @project = projects('projects_001')
+    @project.enabled_module_names = [:glossary]
+    roles('roles_001').add_permission! :view_terms, :manage_terms
+  end
+
+  def test_index
+    @request.session[:user_id] = users('users_002').id
+    get :index, params: { project_id: 1 }
+    assert_response :success
   end
 end
